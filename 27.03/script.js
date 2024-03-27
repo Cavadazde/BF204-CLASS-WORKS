33468d63046265f610123f8e103d439b8fb0a6e3
Cavadzade Aysel <aysel.cavadzade.04@mail.ru>
2024-03-27 20:00:30 +0400

    Javascript

---
+++ 27.03/script.js
@@ -0,0 +1,51 @@
+// let second= window.prompt("Enter second:");
+// console.log((parseInt(second/60)+ " minute " + (second%60) + " second"));   task1
+
+
+
+// let temperatur=window.prompt("Enter temperatur"); task2
+// console.log(parseFloat(5/9 * (temperatur-32)));
+
+
+
+// let a, b, c, d, e;
+// a=Number(window.prompt("enter first num"));
+// b=+window.prompt("enter second num");
+// c=+window.prompt("enter third num");
+// d=+window.prompt("enter fourth num");
+// e=+window.prompt("enter fifth num");
+
+// let result = (a + b + c + d + e) / 5;
+// console.log(result); task3
+
+
+// Daxil edilən doğum ilinə görə yaşı hesablayn proqram yazın.
+
+// let date= Number(window.prompt("Year"));
+// console.log(2024-date); task4;
+
+// Ədədin faizini tapan proqram yazın.
+
+// let eded, faiz;
+// eded=Number(window.prompt("enter eded"));
+// faiz=Number(window.prompt("enter faiz"));
+
+// console.log((eded / 100  ) * faiz);  task5
+
+
+// Daxil edilən katetlərə görə düzbucaqlı üçbucağın sahəsini tapan proqram yazın.
+
+// let a, b;
+// a=Number(window.prompt("enter first num"));
+// b=Number(window.prompt("enter second num"));
+// console.log(a * b / 2); task6
+
+
+// İstifadəçidən alınan kredit miqdarı,faiz dərəcəsi və kredit müddəti əsasında aylar üzrə nə qədər ödəniş etməli olduğunu tapan proqram yazın.
+// let a, b, c;
+
+// a=Number(window.prompt("enter first num"));
+// b=Number(window.prompt("enter second num"));
+// c=Number(window.prompt("enter third num"));
+
+// console.log((a + a*b/100)/c);task7
